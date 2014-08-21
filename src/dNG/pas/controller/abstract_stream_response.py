3ba7c54466b1ada6547440922b7d7fdbd8226084
NotTheEvilOne <git-NTEO@vplace.de>
2014-08-21 20:51:32 +0200

    Trivial: Removed file extension from URLs in the file header

--- src/dNG/pas/controller/abstract_stream_response.py
+++ src/dNG/pas/controller/abstract_stream_response.py
@@ -6,13 +6,13 @@ direct PAS
 Python Application Services
 ----------------------------------------------------------------------------
 (C) direct Netware Group - All rights reserved
-http://www.direct-netware.de/redirect.py?pas;server
+https://www.direct-netware.de/redirect?pas;server
 
 This Source Code Form is subject to the terms of the Mozilla Public License,
 v. 2.0. If a copy of the MPL was not distributed with this file, You can
 obtain one at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------------------------
-http://www.direct-netware.de/redirect.py?licenses;mpl2
+https://www.direct-netware.de/redirect?licenses;mpl2
 ----------------------------------------------------------------------------
 #echo(pasServerVersion)#
 #echo(__FILEPATH__)#
@@ -35,7 +35,7 @@ A stream response reads data from a streamer and writes it to a response object.
 :package:    pas
 :subpackage: server
 :since:      v0.1.00
-:license:    http://www.direct-netware.de/redirect.py?licenses;mpl2
+:license:    https://www.direct-netware.de/redirect?licenses;mpl2
              Mozilla Public License, v. 2.0
 	"""
 
